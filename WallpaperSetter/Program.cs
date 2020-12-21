8fa7a9916f2b4215f5bd13a9a426651e87ebf2da
Nicholas M Martin <nmmart04@louisville.edu>
2020-12-21 11:50:25 -0500

    Add project files.

---
+++ WallpaperSetter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WallpaperSetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wallpaper URI: ");
+            var input = Console.ReadLine();
+
+            try
+            {
+                var uri = new Uri(input);
+
+                Wallpaper.Set(uri, Wallpaper.Style.Stretched);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WallpaperSetter/Wallpaper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using Microsoft.Win32;
+
+namespace WallpaperSetter
+{
+    public static class Wallpaper
+    {
+        private const int SPI_SETDESKWALLPAPER = 20;
+        private const int SPIF_UPDATEINIFILE = 0x01;
+        private const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(Uri uri, Style style)
+        {
+            var tempPath = DownloadImageFromUriToTempPath(uri);
+
+            var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+
+            if (key == null)
+                throw new ArgumentNullException(nameof(key),"Failed to find Desktop Wallpaper registry key!");
+
+            switch (style)
+            {
+                case Style.Tiled:
+                    key.SetValue(@"WallpaperStyle", 1.ToString());
+                    key.SetValue(@"TileWallpaper", 1.ToString());
+                    break;
+                case Style.Centered:
+                    key.SetValue(@"WallpaperStyle", 1.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                case Style.Stretched:
+                    key.SetValue(@"WallpaperStyle", 2.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(style), style, "Style does not exist!");
+            }
+
+            // Set in system
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        private static string DownloadImageFromUriToTempPath(Uri uri)
+        {
+            var s = new WebClient().OpenRead(uri.ToString());
+            using var img = Image.FromStream(s ?? throw new InvalidOperationException("Failed to download image from URI!"));
+            var tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, ImageFormat.Bmp);
+            return tempPath;
+        }
+    }
+
+}
